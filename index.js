3692266ce1f037d6366afb5898c69abe7be04612
Gantlas <3glifeyy@gmail.com>
2020-08-07 18:12:22 +0300

    forgot to create new branch(

--- index.js
+++ index.js
@@ -1,88 +1,110 @@
-function isCorrectName(name) {
+const init = () => {
+  const surname = getInfo("Enter your surname", isCorrectName);
+  const firstName = getInfo("Enter your first name", isCorrectName);
+  const middleName = getInfo("Enter your middle name", isCorrectName);
+  const age = getInfo("Enter your age", isCorrectNumber);
+  let birthdayString = null;
+  let isCorrectAge = false;
+  let daysAge = null;
+
+  while (!isCorrectDate(birthdayString) || !isCorrectAge) {
+    birthdayString = String(
+      prompt(
+        `Enter your date of birth (example: 23.12.1998)
+Your age: ${age}`,
+        "dd.mm.yyyy"
+      )
+    );
+
+    const birthdayArray = birthdayString.split(".");
+
+    let todaysDate = new Date();
+    const dateOfBirth = new Date(
+      birthdayArray[2],
+      birthdayArray[1] - 1,
+      birthdayArray[0]
+    );
+
+    daysAge = ageInDays(todaysDate, dateOfBirth);
+    isCorrectAge = compareAge(todaysDate, dateOfBirth, birthdayArray, age);
+  }
+
+  const isMale = confirm("Is your gender male?");
+
+  const gender = isMale ? "male" : "female";
+
+  print(surname, firstName, middleName, age, gender, isPensioner, daysAge);
+};
+
+const isCorrectName = (name) => {
   const regForName = /[^a-z]/i;
   return !regForName.test(name) && !!name;
-}
+};
 
-function isCorrectNumber(num) {
+const isCorrectNumber = (num) => {
   const regForNumber = /[^\d]/i;
-  return !regForNumber.test(num) && !!num;
-}
+  return !regForNumber.test(num) && !!num && +num;
+};
 
-function isCorrectDate(str) {
+const isCorrectDate = (str) => {
   const regForDate = /^\d\d\.\d\d\.\d\d\d\d$/;
   return regForDate.test(str);
-}
+};
 
-function getInfo(outputText, func) {
+const getInfo = (outputText, func) => {
   let data = prompt(outputText, "");
   while (!func(data)) {
     data = prompt(`!!! Invalid input !!! ${outputText}`, "");
   }
   return data[0].toUpperCase() + data.slice(1);
-}
+};
 
-const surname = getInfo("Enter your surname", isCorrectName);
-const firstName = getInfo("Enter your first name", isCorrectName);
-const middleName = getInfo("Enter your middle name", isCorrectName);
-const age = getInfo("Enter your age", isCorrectNumber);
-let birthdayString = null;
-let isCorrectAge = false;
-let daysAge = null;
+const ageInDays = (today, birthday) => {
+  let age = Math.floor((today - birthday) / 86400000);
+  return age;
+};
 
-while (!isCorrectDate(birthdayString) || !isCorrectAge) {
-  birthdayString = String(
-    prompt(
-      `Enter your date of birth (example: 23.12.1998)
-Your age: ${age}`,
-      "dd.mm.yyyy"
-    )
-  );
-
-  const birthdayArray = birthdayString.split(".");
-
-  let todaysDate = new Date();
-  const dateOfBirth = new Date(
-    birthdayArray[2],
-    birthdayArray[1] - 1,
-    birthdayArray[0]
-  );
-
-  daysAge = Math.floor((todaysDate - dateOfBirth) / 86400000);
-
+const compareAge = (today, birthday, birthArr, age) => {
   let calculatedAge = null;
-  if (
-    todaysDate <
-    new Date(todaysDate.getFullYear(), birthdayArray[1] - 1, birthdayArray[0])
-  ) {
-    calculatedAge = todaysDate.getFullYear() - dateOfBirth.getFullYear() - 1;
+  if (today < new Date(today.getFullYear(), birthArr[1] - 1, birthArr[0])) {
+    calculatedAge = today.getFullYear() - birthday.getFullYear() - 1;
   } else {
-    calculatedAge = todaysDate.getFullYear() - dateOfBirth.getFullYear();
+    calculatedAge = today.getFullYear() - birthday.getFullYear();
   }
 
-  isCorrectAge = calculatedAge === +age;
-}
+  return calculatedAge === +age;
+};
 
-const isMale = confirm("Is your gender male?");
+const isPensioner = (gender, age) => {
+  if (gender === "male" && age >= 65) {
+    return true;
+  } else if (gender === "female" && age >= 55) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-const gender = isMale ? "male" : "female";
-let isPensioner = null;
-
-if (gender === "male" && age >= 65) {
-  isPensioner = true;
-} else if (gender === "female" && age >= 55) {
-  isPensioner = true;
-} else {
-  isPensioner = false;
-}
-
-alert(
-  `Your full name: ${surname} ${firstName} ${middleName}
+const print = (
+  surname,
+  firstName,
+  middleName,
+  age,
+  gender,
+  isPensioner,
+  daysAge
+) => {
+  alert(
+    `Your full name: ${surname} ${firstName} ${middleName}
 Your age: ${age}
 You will be ${+age + 5} in 5 years
 Your gender: ${gender}
-You are ${isPensioner ? "pensioner" : "not a pensioner"}
+You are ${isPensioner(gender, age) ? "pensioner" : "not a pensioner"}
 You have lived full days: ${daysAge}`
-);
+  );
+};
+
+init();
 
 /*************************** Подсчет русских гласных в строке ************************************/
 // const string = prompt("Enter the string", "");
@@ -94,6 +116,25 @@ You have lived full days: ${daysAge}`
 // } else {
 //   const arrOfVowels = string.match(re);
 //   count = !arrOfVowels ? 0 : arrOfVowels.length;
+// }
+
+// console.log(count);
+
+/*************************** Подсчет русских гласных в строке (for)************************************/
+// const string = prompt("Enter the string", "");
+// const arrOfVowels = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"];
+// let count = 0;
+
+// for (let i = 0; i < string.length; i++) {
+//   for (let j = 0; j < arrOfVowels.length; j++) {
+//     if (
+//       string[i] === arrOfVowels[j] ||
+//       string[i] === arrOfVowels[j].toUpperCase()
+//     ) {
+//       count++;
+//       break;
+//     }
+//   }
 // }
 
 // console.log(count);
